3b2f641ede55bf4d7ac04644bbfcd0eb30cb1bef
”REIKO-SATO” <lingziz315@gmail.com>
2020-09-07 14:07:27 +0000

    first commit

---
+++ app/models/topic.rb
@@ -0,0 +1,14 @@
+class Topic < ApplicationRecord
+  validates :user_id, presence: true
+  validates :date,presence: true
+  validates :brand,presence: true
+  validates :item,presence: true
+  validates :size,presence: true
+  validates :price,presence: true
+  validates :description,presence: true, length: { maximum: 30 }
+  validates :image,presence: true
+  
+  belongs_to :user
+  
+  mount_uploader :image, ImageUploader
+end
